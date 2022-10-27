57371010a97c633d498367955fe122476c840e98
RobWar-code <rowaniar@aol.com>
2022-10-27 06:13:18 +0000

    Start javascript recipe data

---
+++ assets/js/recipe-data.js
@@ -0,0 +1,72 @@
+// Author: Robin Warner
+// Recipe Data object for Culinary Delight
+const weeklyRecipes = [
+    {
+        weekNumber: 1,
+        courses: [
+            {
+                course: "main",
+                title: "Special Cottage Pie"
+            },
+            {
+                course: "dessert",
+                title: "Apple Tarte Tatin"
+            }
+        ]
+    }
+];
+
+// Recipes
+/* Recipe Template
+    {
+        title: "", // String the name of the dish
+        course: "main", // String, "main", "dessert" or "starter"
+        description: "", // String, 1 to three sentences describing the meal
+        credits: "", // String, where the recipe came from, "" if not applicable
+        ingredients: [
+            "", // String, ingredient item
+        ],
+        utensils: [
+            "", // String, utensil item
+        ],
+        steps: [
+            [
+                "", // String one or more paragraphs for recipe step
+            ]
+        ]
+    }
+*/
+const recipe = [
+    {
+        title: "Special Cottage Pie",
+        course: "main",
+        description: "An extra tasty version of the home standard with some posh finishing touches",
+        credits: "Our wonderful in-house chef - Invisible Alice",
+        ingredients: [
+            "1/2 tin chopped tomatoes",
+            "1 large onion",
+            "2 medium-sized carrots",
+            "1/2 garlic bulb",
+            "1 teaspoon dried mixed herbs",
+            "1/2 teaspoon ground black pepper",
+            "100g cheddar cheese",
+            "450g minced beef",
+            "3 or 4 average-sized potatoes",
+            "1 fresh tomato",
+            "Salt",
+            "Knob of margarine or butter",
+            "Two tablespoons of cooking oil",
+            "1 medium-sized cauliflower (accompaniment)"
+        ],
+        utensils: [
+            "2 medium-sized saucepans",
+            "Large frying pan",
+            "1 9-inch baking tin"
+        ],
+        steps: [
+            [
+                
+            ]
+        ]
+    }
+];
